8fd4b680b8f82734d4916d7efcf5e37b89da8670
Eric Eastwood <madlittlemods@gmail.com>
2013-01-04 17:34:27 -0600

    Fixed bugs!

--- FirstRobotEver/src/org/usfirst/frc0/commands/CommandBase.java
+++ FirstRobotEver/src/org/usfirst/frc0/commands/CommandBase.java
@@ -16,7 +16,7 @@ public abstract class CommandBase extends Command {
 
     public static OI oi;
     // Create a single static instance of all of your subsystems
-    public static DriveTrain driveTrain = new DriveTrain(SmartDashboard.getInt("driveType"));
+    public static DriveTrain driveTrain = new DriveTrain(SmartDashboard.getInt("driveType", 1));
 
     public static void init() {
         // This MUST be here. If the OI creates Commands (which it very likely
